f4bae2deee7fb19f86816d8a9cfb2e8991c59027
CihatAltiparmak <cihataltiparmak1@gmail.com>
2024-03-17 16:45:21 +0300

    Formatted launch files for solving CI/CD issues

--- mit_racecar_gazebo/launch/mit_racecar_spawn.launch.py
+++ mit_racecar_gazebo/launch/mit_racecar_spawn.launch.py
@@ -1,25 +1,30 @@
 import os
-from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration, Command
-from launch_ros.substitutions import FindPackageShare
-from launch_ros.actions import Node
 from launch.actions import SetEnvironmentVariable
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
     pkg_share = FindPackageShare(package='mit_racecar_description').find('mit_racecar_description')
     urdf_file = os.path.join(pkg_share, 'urdf/racecar.xacro')
 
-    gazebo_env = SetEnvironmentVariable("GAZEBO_MODEL_PATH", pkg_share)
+    SetEnvironmentVariable('GAZEBO_MODEL_PATH', pkg_share)
 
     return LaunchDescription([
-        DeclareLaunchArgument('start_x', default_value='0.0', description='X coordinate of starting position'),
-        DeclareLaunchArgument('start_y', default_value='0.0', description='Y coordinate of starting position'),
-        DeclareLaunchArgument('start_z', default_value='0.0', description='Z coordinate of starting position'),
-        DeclareLaunchArgument('start_yaw', default_value='0.0', description='Yaw angle of starting orientation'),
-        DeclareLaunchArgument('robot_name', default_value='', description='Name and prefix for this robot'),
+        DeclareLaunchArgument('start_x', default_value='0.0',
+                              description='X coordinate of starting position'),
+        DeclareLaunchArgument('start_y', default_value='0.0',
+                              description='Y coordinate of starting position'),
+        DeclareLaunchArgument('start_z', default_value='0.0',
+                              description='Z coordinate of starting position'),
+        DeclareLaunchArgument('start_yaw', default_value='0.0',
+                              description='Yaw angle of starting orientation'),
+        DeclareLaunchArgument('robot_name', default_value='',
+                              description='Name and prefix for this robot'),
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
@@ -27,7 +32,8 @@ def generate_launch_description():
             output='screen',
             parameters=[{
                 'use_sim_time': True,
-                'robot_description': Command([f'xacro {urdf_file}', ' robot_name:=', LaunchConfiguration('robot_name')])
+                'robot_description': Command(
+                    [f'xacro {urdf_file}', ' robot_name:=', LaunchConfiguration('robot_name')])
             }]),
         Node(
             package='gazebo_ros',
